e4a4d3475f100e36bff21a074aa87987ea27c0b5
Nice <nicenicegame@gmail.com>
2021-03-08 14:10:15 +0700

    Added discount

--- app.js
+++ app.js
@@ -2,15 +2,35 @@ const input = document.querySelector('input[type="range"]')
 const animateSlider = document.querySelector('.slider-animate')
 const checkbox = document.querySelector('input[type="checkbox"]')
 const checkboxBg = document.querySelector('.checkbox')
+const priceText = document.querySelector('.price span')
 
-checkbox.addEventListener('click', () => {
+let price = input.value
+let originalPrice = null
+
+checkbox.addEventListener('input', () => {
+  if (checkbox.checked) {
+    originalPrice = price
+    price = price * 0.75
+  } else {
+    price = originalPrice
+    originalPrice = null
+  }
+  priceText.innerText = `$${price}.00`
   checkboxBg.classList.toggle('active')
 })
 
-animateSlider.style.width = `${100 - ((input.value - 8) / 24) * 100}%`
+animateSlider.style.width = `${100 - ((price - 8) / 24) * 100}%`
 
 input.addEventListener('input', () => {
-  const scale = input.value
-  console.log(scale)
-  animateSlider.style.width = `${100 - ((scale - 8) / 24) * 100}%`
+  price = input.value
+  animateSlider.style.width = `${100 - ((price - 8) / 24) * 100}%`
+  if (checkbox.checked) {
+    originalPrice = price
+    price = price * 0.75
+  } else {
+    if (!originalPrice) {
+      price = input.value
+    }
+  }
+  priceText.innerText = `$${price}.00`
 })
