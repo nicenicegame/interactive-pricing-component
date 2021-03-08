9382e6706553580049dd42b796d0e8a3e7c449d5
Nice <nicenicegame@gmail.com>
2021-03-08 13:20:44 +0700

    Style overall ui

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ checkbox.addEventListener('click', () => {
   checkboxBg.classList.toggle('active')
 })
 
-animateSlider.style.width = `${100 - input.value}%`
+animateSlider.style.width = `${100 - ((input.value - 8) / 24) * 100}%`
 
 input.addEventListener('input', () => {
   const scale = input.value
-  animateSlider.style.width = `${100 - scale}%`
+  console.log(scale)
+  animateSlider.style.width = `${100 - ((scale - 8) / 24) * 100}%`
 })
